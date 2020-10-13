2ccf9be01584d3180ab2d901bb15487baa9bb8d0
Wesley Matos <wrickee@gmail.com>
2020-10-13 11:15:17 -0300

    feat: make implementation of function

--- t8.py
+++ t8.py
@@ -4,16 +4,28 @@ from math import sqrt
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def nao_entre_em_panico():
+    numbers = []
 
+    p = 1
 
-    primos = "Tudo vai dar certo caros alunos!"
+    while p <= 100:
+        is_prime = True
+        for i in range(2, p):
+            if (p % i) == 0:
+                is_prime = False
+                break
 
+        if is_prime:
+            numbers.append(f"{p}")
 
-    return primos
+        p += 1
+
+    return "<br>".join(list(dict.fromkeys(numbers)))
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
-
